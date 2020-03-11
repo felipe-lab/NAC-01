a6cc7d1a0863547d31ed759def8e93c83cee3b08
felipe-lab <61830873+felipe-lab@users.noreply.github.com>
2020-03-11 19:14:23 -0300

    Classe Mensagem.
    Variáveis e métodos.

--- Documents/calc/BoasVindas.java
+++ Documents/calc/BoasVindas.java
@@ -5,21 +5,24 @@ public class BoasVindas {
 	private String mensagem;
 
 	// Construtor de Objeto na Classe
-	public BoasVindas(String mens) {
-		this.mensagem = mens;
-	}
+	public BoasVindas(String mens) {this.mensagem = mens;}
 
-	// Exibição da mensagem
+	// ExibiÃ§Ã£o da mensagem
 	public String exibirMensagem() {
+		
 		System.out.println(this.mensagem);
+		
 		return this.mensagem;
 	}
 
-	// Exibição da parte fixa da mensagem
+	// ExibiÃ§Ã£o da parte fixa da mensagem
 	public String Mensagem() {
+		
 		String mens;
 		mens = "Seja bem vindo a sua calculadora pessoal!";
+		
 		System.out.println(mens);
+		
 		return mens;
 	}
 
